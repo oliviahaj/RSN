d5645560467f9ad28954ea760ccbc18dc7d1ab10
Olivia Hajek <52761668+oliviahaj@users.noreply.github.com>
2026-07-01 09:43:10 -0600

    Create all files needed for CAFI ages

--- cafi_age_checks.R
+++ cafi_age_checks.R
@@ -1,6 +1,9 @@
 # 1) Cleaning John Yarie's tree ring data to create a file with all of the trees and pith years 
-# 2) Creating a summary file of plot ages
-# - this file will correspond to ages identified in Melissa Boyd's file, so there will be some trees that are removed from calculating a site average
+# 2) Creating an updated file with all of the CAFI tree rings available
+# 3) Creating a summarized file of CAFI plot ages - across all of 
+# - this file will correspond to ages identified in Melissa Boyd's file, 
+#   so there will be some trees that are removed from calculating a site average
+# - this will also include the fire data where relevant from Adrianna
 
 ## OLH
 ## June 30
@@ -62,7 +65,7 @@ cafi.cores.yarie <- tree.2 %>%
     TRUE ~ 'NA'
   )) %>%
   mutate(YearSampled = YearSampled + 2000, TreeYear = YearSampled - AGE + 1, PithPresent = NA, 
-         source = "AK_PSP_increment_cores USU Mar 2013b" ,Windendro_Compiler = "John Yarie, Melissa Boyd",
+         Source = "AK_PSP_increment_cores USU Mar 2013b" ,Windendro_Compiler = "John Yarie, Melissa Boyd",
          # update age to be for 2026?
          AGE = 2026 - TreeYear)  %>%
   # add notes to the trees not included in teh averaging
@@ -82,50 +85,54 @@ cafi.cores.yarie <- tree.2 %>%
     TRUE ~ Notes
   )) %>%
   # included or removed
-  mutate(Remove = ifelse(SampleNumber %in% c("166-094", "305-094","088-095","087-095", "294-370","316-370", "327-370"), 1, 0))
+  mutate(Remove = ifelse(SampleNumber %in% c("166-094", "305-094","088-095","087-095", "294-370","316-370", "327-370"), 1, 0)) %>%
+  rename(Plot = PLOT)
 
 # export this file to be joined
 write.csv(cafi.cores.yarie, "/Users/olhajek/Desktop/RSN/CAFI_data/CAFI_Boyd_061522/CAFI_stand_age/cafi_cores_yarie_aged.csv", row.names = FALSE)
 
 
-# 4) Filter plots where the ages are not just averages of all trees 
-tree.32 <- tree.2 %>%
-  left_join(unique.subs)%>%
-  filter(PLOT %in% c(1180, 1096, 1175,1183))
-# all but 1180 are just an average of the two oldest trees adn then the timepoint. not sure what's going on wiht 1180
+# 4) Join with Zach's merged CAFI file that also has Isabelle and Andrew's Cores
 
-tree.3 <- tree.2 %>%
-  left_join(unique.subs) %>%
-  group_by(PLOT, Spp) %>%
-  summarize(mean = mean(`Count/Years`), med = median(`Count/Years`),max = max(`Count/Years`),min = min(`Count/Years`), n = n())%>%
-  left_join(mel.age) %>%
-  mutate(diff = age_biomass_yr_msrd - mean) %>%
-  filter(diff > 1 | diff < -1)
+cafi.cores <- read.csvcafi.cores <- read.csv("/Users/olhajek/Desktop/RSN/Tree_Age_Data/Compiled/CAFI_Tree_Age_Compiled.csv")
 
-ggplot(tree.3, aes(diff))+
-  geom_histogram()+
-  facet_wrap(~CAFI_sampling_time)
+# 5) Update the CAFI file to remove the current Melissa data and replace it with the new tree cores from John Yarie
+cafi.cores2 <- cafi.cores %>%
+  filter(Source != "age_cafi_021621") %>%
+  mutate(Remove = 0) %>%
+  rbind(cafi.cores.yarie) %>%
+  # Give Andrew's a tree year because they were all sampled in 2025, but going to make it 2026 for consistency with Boyd
+  mutate(TreeYear = ifelse(Source == "CAFI_Tree_Cores_2025", 2025 - AGE +1, TreeYear))
 
-tree.4 <- tree.2 %>%
-  left_join(unique.subs) %>%
-  group_by(PLOT) %>%
-  summarize(mean = mean(`Count/Years`), max = max(`Count/Years`),min = min(`Count/Years`))%>%
-  left_join(mel.age)%>%
-  mutate(diff = age_biomass_yr_msrd - mean)
+# 6) Save this compiled file with all of teh tree ages
+write.csv(cafi.cores2, "/Users/olhajek/Desktop/RSN/Tree_Age_Data/Compiled/CAFI_Tree_Age_Compiled_OH.csv", row.names = FALSE)
+
+# 7) Use this file to create an update, compiled CAFI age data
+# - want to compare ages across sampling cores/efforts and see what sort of overlap there is
+str(cafi.cores2)
+
+cafi.ages <- cafi.cores2 %>%
+  mutate(Remove = ifelse(Notes == "Strongly recommend omitting from data. Very fractured, P2 crosses through bark and extensive scar, not counted (or final segments connected)",
+                         1, Remove)) %>%
+  mutate(Remove = ifelse(is.na(Remove), 0, 1))%>%
+  filter(Remove != 1) %>%
+  group_by(Plot, YearSampled, Windendro_Compiler) %>%
+  summarize(n = n(), mean_ty = mean(TreeYear, na.rm = TRUE), min_ty = min(TreeYear), max_ty = max(TreeYear), 
+            mean_AGE = mean(AGE, na.rm = TRUE), median_AGE = median(AGE, na.rm = TRUE), min_AGE = min(AGE), max_AGE = max(AGE)) %>%
+  mutate(range_AGE = max_AGE - min_AGE, mean_med = mean_AGE - median_AGE) %>%
+  filter(!is.na(Plot))
+
+# How many plots have multiple sampling efforts
+sample.num <- cafi.ages %>%
+  ungroup() %>%
+  select(c(Plot, Windendro_Compiler)) %>%
+  unique() %>%
+  group_by(Plot) %>%
+  summarize(n=n())
+# only 2 plots were sampled multiple times - 1022 and 1088
+# 1088 is quite different - see if it burned?
+# check with the raw data on these...see if there is any indication of fire? can look at trees
 
 
-subp <- read.csv("/Users/olhajek/Desktop/RSN/CAFI_data/452_CAFI_SUBPLOT_v20241220.csv")
-
-str(subp)
-subp.prep <- subp %>%
-  select(PLOT, CYCLE, MEASYEAR) %>%
-  mutate(CAFI_sampling_time = paste("T",CYCLE, sep="")) %>%
-  distinct()
-
-test <- left_join(tree.3, subp.prep)  
-
-test.2 <- test %>%
-  mutate(year2 = Year + 2000, 
-         diff2 =  MEASYEAR - year2, 
-         off = diff2 - diff, 
-         range = max-min)
+# 8) Save the plot ages summary
+write.csv(cafi.ages, "/Users/olhajek/Desktop/RSN/Tree_Age_Data/Compiled/CAFI_Ages_Summarized.csv",row.names = FALSE)
